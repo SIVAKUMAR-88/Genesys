d4cb83fd2959fd5ac6048ada81138a96fb8b2175
SIVAKUMAR-88 <63393917+SIVAKUMAR-88@users.noreply.github.com>
2025-12-08 18:15:06 +0400

    Update variables.tf

--- blueprint/variables.tf
+++ blueprint/variables.tf
@@ -1,13 +1,5 @@
 
-variable "genesys_email_domain" {
-  type        = string
-  description = "The name of the email domain  This is used to help build the email route"
-}
 
-variable "genesys_email_domain_region" {
-  type        = string
-  description = "The name of the email region that the email domain name will reside in."
-}
 
 variable "classifier_url" {
   type        = string
